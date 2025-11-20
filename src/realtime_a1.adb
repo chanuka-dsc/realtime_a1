27566fe0b6e1d055d8861867733e0c33defb56ac
chanuka-dsc <85240799+chanuka-dsc@users.noreply.github.com>
2025-11-20 14:51:40 +0100

    run with realtime_a1.adb

--- src/realtime_a1.adb
+++ src/realtime_a1.adb
@@ -1,4 +1,9 @@
+with cyclic;  -- assuming A.adb contains "procedure A is"
+
+
 procedure Realtime_A1 is
 begin
-   null;
+   -- Call whatever you want to run:
+   cyclic;
+-- or B; or C; etc.
 end Realtime_A1;
